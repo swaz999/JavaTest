8b9e7c3801770a063f514e9ced30f37bea364523
swaz999 <Swathi.Vangala@charter.com>
2019-08-26 16:10:33 -0600

    Addition of two integers

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,8 @@
 public class MainClass {
     public static void main(String[] args) {
+        int a=4;
+        int b = 6;
+        System.out.println("Result:"+(a+b));
         System.out.println("Hello world from Java-Maven IntelliJ project");
         System.out.println("Learn java in 2 months");
 
