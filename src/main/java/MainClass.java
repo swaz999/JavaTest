b671a509d06686b67d32e741c2ed472a5fdd7aff
swaz999 <Swathi.Vangala@charter.com>
2019-08-26 16:08:29 -0600

    ignore

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello world from Java-Maven IntelliJ project");
+        System.out.println("Learn java in 2 months");
 
     }
 }
